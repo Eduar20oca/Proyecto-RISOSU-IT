de5c357dfc265d72c56707f0bec05837131cc2d4
Eduar20oca <jeocampo01@outlook.com>
2025-07-10 10:40:30 -0600

    Busqueda abierta

--- PL/Controllers/UsuarioController.cs
+++ PL/Controllers/UsuarioController.cs
@@ -7,17 +7,18 @@ namespace PL.Controllers
         private readonly BL.Usuario _usuario;
         private readonly BL.Rol _rol;
 
-        public UsuarioController(BL.Usuario usuario , BL.Rol rol)
+        public UsuarioController(BL.Usuario usuario, BL.Rol rol)
         {
             _usuario = usuario;
             _rol = rol;
         }
 
-
-        public IActionResult GetAll(ML.Usuario usuario)
+        [HttpGet]
+        public IActionResult GetAll()
         {
+            ML.Usuario usuario = new ML.Usuario();
             usuario.Rol = new ML.Rol();
-            ML.Result result =_usuario.GetAllSPEF(usuario);          
+            ML.Result result = _usuario.GetAllSPEF(usuario);
 
             if (result.Correct)
             {
@@ -31,10 +32,35 @@ namespace PL.Controllers
                 usuario.Rol.Roles = resultRol.Objects;
             }
 
-
-            
-            
             return View(usuario);
         }
+
+        [HttpPost]
+        public IActionResult GetAll(ML.Usuario Usuario)
+        {
+            Usuario.Rol = new ML.Rol();
+
+            Usuario.Nombre = Usuario.Nombre ?? "";
+            Usuario.ApellidoPaterno = Usuario.ApellidoPaterno ?? "";
+            Usuario.ApellidoMaterno = Usuario.ApellidoMaterno ?? "";
+
+            ML.Result result = _usuario.GetAllSPEF(Usuario);
+
+            if (result.Correct)
+            {
+                Usuario.Usuarios = result.Objects;
+            }
+
+            ML.Result resultRol = _rol.GetAll();
+            if (resultRol.Correct)
+            {
+                Usuario.Rol.Roles = resultRol.Objects;
+            }
+
+            return View(Usuario);
+        }
+
+
+
     }
 }
